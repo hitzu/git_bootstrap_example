287586b7557617bf4cd9b91dcf27b7c315819ee1
Hitzu <robert.torres.lopez@gmail.com>
2016-07-31 10:57:26 -0500

    se corrigio error en pagina principal (alineado incorrecto)

--- index.php
+++ index.php
@@ -31,26 +31,28 @@
 				</div>
 			</div>
 		</article>
-		<article id="w2" class="workshop-right">
-			<div class="row">
-					<div class="col-xs-3 date-info">
-						<h3 class="highlighted"><strong>W38</strong></h3>
-						<h3 class="highlighted">15 plazas</h3>
-						<h3 class="highlighted">120 E</h3>
-					</div>
-					<div class="col-xs-8 col-xs-offset-1 main-info">
-						<h2>
-							Desarrolla Aplicaciones multiplataforma con AIR
-						</h2>
-						<p>
-							Sorprendete de lo facil que puede ser crear apps para iOS y android
-						</p>
-						<a href="detail.php" class="btn btn-custom">ME APUNTO!</a>
-						<a href="detail.php" class="more-info-link">Quiero más información</a>
-					
-					</div>
-			</div>
-		</article>
+		<div class="container">
+			<article id="w2" class="workshop-right">
+				<div class="row">
+						<div class="col-xs-3 date-info">
+							<h3 class="highlighted"><strong>W38</strong></h3>
+							<h3 class="highlighted">15 plazas</h3>
+							<h3 class="highlighted">120 E</h3>
+						</div>
+						<div class="col-xs-8 col-xs-offset-1 main-info">
+							<h2>
+								Desarrolla Aplicaciones multiplataforma con AIR
+							</h2>
+							<p>
+								Sorprendete de lo facil que puede ser crear apps para iOS y android
+							</p>
+							<a href="detail.php" class="btn btn-custom">ME APUNTO!</a>
+							<a href="detail.php" class="more-info-link">Quiero más información</a>
+						
+						</div>
+				</div>
+			</article>
+		</div>
 	</div>
 
 	<?php include 'inc/footer.php'; ?>
