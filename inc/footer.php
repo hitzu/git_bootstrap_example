762fcfa1273c5dfd142d73983564e72b6135c866
Hitzu <robert.torres.lopez@gmail.com>
2016-07-31 16:26:02 -0500

    modificacion y estilizacion del foooter

--- inc/footer.php
+++ inc/footer.php
@@ -1,37 +1,39 @@
-<div class="container">
-	<div class="row">
-		<div class="col-xs-5">
-			<h3>Proximos WORKSHOPS</h3>
-			<ul>
-				<li><a href="#">Primer WorkShop</a></li>
-				<li><a href="#">Segundo WorkShop</a></li>
-			</ul>
-		</div>
-		<div class="col-xs-6 col-xs-offset-1">
-			<h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
-			<form id="subscribe-form" action="">
-				<div class="row">
-					<div class="col-xs-8">
-						<div class="form-group">
-							
-							<input type="text" id="email" name="email" class="form-control input-lg" placeholder="tunombre@tudominio">
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-5" id="next-workshops"> 
+				<h3>Proximos WORKSHOPS</h3>
+				<ul>
+					<li><a href="#">Primer WorkShop</a></li>
+					<li><a href="#">Segundo WorkShop</a></li>
+				</ul>
+			</div>
+			<div class="col-xs-6 col-xs-offset-1">
+				<h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
+				<form id="subscribe-form" action="">
+					<div class="row">
+						<div class="col-xs-8">
+							<div class="form-group">
+								
+								<input type="text" id="email" name="email" class="form-control input-lg" placeholder="tunombre@tudominio">
+							</div>
+						</div>
+						<div class="col-xs-4">
+							<button class="btn btn-custom btn-lg">SUSCRIBIRME</button>
 						</div>
 					</div>
-					<div class="col-xs-4">
-						<button class="btn btn-custom btn-lg">SUSCRIBIRME</button>
-					</div>
-				</div>
-			</form>
-		</div>
-		<div class="clearfix"></div>
-		<div class="col-xs-5">
-			<h3>SOBRE LOS WORKSHOPS</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At vero eum ducimus recusandae debitis maiores.</p>
-		</div>
-		<div class="col-xs-6 col-xs-offset-1">
-			<h3>¿TE GUSTA? CORRE LA VOZ</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non veritatis laboriosam, voluptatibus amet nulla expedita natus officia nisi rem dolore ipsum et magni explicabo, voluptatem.</p>
-			<!-- insertar los elementos-->
+				</form>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-xs-5">
+				<h3>SOBRE LOS WORKSHOPS</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At vero eum ducimus recusandae debitis maiores.</p>
+			</div>
+			<div class="col-xs-6 col-xs-offset-1">
+				<h3>¿TE GUSTA? CORRE LA VOZ</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non veritatis laboriosam, voluptatibus amet nulla expedita natus officia nisi rem dolore ipsum et magni explicabo, voluptatem.</p>
+				<!-- insertar los elementos-->
+			</div>
 		</div>
 	</div>
-</div>
+</footer>
