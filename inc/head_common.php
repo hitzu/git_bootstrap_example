ddcb5e28120ddfcd25ae43a6f56f085780286cdd
Hitzu <robert.torres.lopez@gmail.com>
2016-07-30 23:29:22 -0500

    Creamos estilos.css y le insertamos estilos generales

--- inc/head_common.php
+++ inc/head_common.php
@@ -1,3 +1,5 @@
 <meta charset="UTF-8">
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link href='https://fonts.googleapis.com/css?family=Source+Code+Pro:200,300,400,500,600,700 subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href='css/style.css' rel='stylesheet' type='text/css'>
